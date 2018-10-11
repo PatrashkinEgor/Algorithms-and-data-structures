527aca4d7a13752bb08a81e77d7130089eaaa938
eapat <eapat@mail.ru>
2018-10-11 12:13:19 +0500

    Дописана функция переворота строки, оставлены коменты.

--- src/main/java/Lesson3HW/LessonThreeHomeWork.java
+++ src/main/java/Lesson3HW/LessonThreeHomeWork.java
@@ -18,108 +18,35 @@ import java.util.Scanner;
  */
 public class LessonThreeHomeWork {
     public static void main(String[] args) {
-
-
-        int command = getCommand("1-Stack, 2-Queue, 3-Deck", 3);
-
+        int command = getCommand("1-Stack, 2-Queue, 3-Deck, 4-String reverse", 4);
         switch (command){
             case 1:
-                Stack stack = new Stack(10);
-                while (true){
-                    int stackCommand = getCommand("1-Push Element, 2-Pop element, 3 - Exit", 3);
-                    switch (stackCommand){
-                        case 1:
-                            if (!stack.isFull()) {
-                                stack.push(getInt());
-                            } else {
-                                System.out.println("Stack is full!");
-                            }
-                            break;
-                        case 2:
-                            if (!stack.isEmpty()){
-                                System.out.println(stack.pop());
-                            }
-                            else {
-                                System.out.println("Stack is empty!");
-                            }
-                            break;
-                        case 3:
-                            System.exit(0);
-                    }
-                    System.out.println(stack.toString());
-                }
+                runStack();
+                break;
             case 2:
-                Queue queue = new Queue(10);
-                while (true){
-                    int queueCommand = getCommand("1-Insert Element, 2-Remove element, 3 - Exit", 3);
-                    switch (queueCommand){
-                        case 1:
-                            if (!queue.isFull()){
-                                queue.insert(getInt());
-                            } else {
-                                System.out.println("Queue is full!");
-                            }
-                            break;
-                        case 2:
-                            if (!queue.isEmpty()) {
-                                System.out.println(queue.remove());
-                            } else {
-                                System.out.println("Queue is empty!");
-                            }
-                            break;
-                        case 3:
-                            System.exit(0);
-                    }
-                    System.out.println(queue.toString());
-                }
+                runQueue();
+                break;
             case 3:
-                Deque deque = new Deque(10);
-                while (true){
-                    int dequeCommand = getCommand("1-Insert left Element, 2-Remove left element, 3-Insert right Element, 4-Remove right element, 5 - Exit", 5);
-                    switch (dequeCommand){
-                        case 1:
-                            if (!deque.isFull()) {
-                                deque.insertLeft(getInt());
-                            } else {
-                                System.out.println("Deque is full!");
-                            }
-                            break;
-                        case 2:
-                            if (!deque.isEmpty()) {
-                                System.out.println(deque.removeLeft());
-                            } else {
-                                System.out.println("Deque is empty!");
-                            }
-                            break;
-                        case 3:
-                            if (!deque.isFull()) {
-                                deque.insertRight(getInt());
-                            } else {
-                                System.out.println("Deque is full!");
-                            }
-                            break;
-                        case 4:
-                            if (!deque.isEmpty()) {
-                                System.out.println(deque.removeRight());
-                            } else {
-                                System.out.println("Deque is empty!");
-                            }
-                            break;
-                        case 5:
-                            System.exit(0);
-                    }
-                    System.out.println(deque.toString());
-                }
+                runDeque();
+                break;
+            case 4:
+                runStringReverse();
+                break;
         }
 
     }
 
-
-   static int getCommand(String message, int numberOfOptions){
+    /**
+     * Метод выводит сообщение и получает номер команды из консоли, при жтом отлавливает эксепшены.
+     * Нужен для выполнения "меню" при выполнении проверки написанных методов.
+     * @param message Выводимое пользователю сообщение
+     * @param numberOfOptions   количество вариантов отвта
+     * @return  номер выбранного варианта
+     */
+    static int getCommand(String message, int numberOfOptions){
        int command;
         Scanner sc = new Scanner(System.in);
         while (true){
-
             try {
                 System.out.println(message);
                 command = sc.nextByte();
@@ -135,6 +62,10 @@ public class LessonThreeHomeWork {
         }
     }
 
+    /**
+     * Метод предназначен для получения целочисленного значения из консоли и отлова исключений.
+     * @return целочисленное значение введенное с консоли.
+     */
     static int getInt(){
         int number;
         Scanner sc = new Scanner(System.in);
@@ -151,5 +82,132 @@ public class LessonThreeHomeWork {
         }
     }
 
+    /**
+     * Метод заполняет дек и забирает из него значения при помощи консоли
+     */
+    static  void runDeque(){
+        Deque deque = new Deque(10);
+        while (true){
+            int dequeCommand = getCommand("1-Insert left Element, 2-Remove left element, 3-Insert right Element, 4-Remove right element, 5 - Exit", 5);
+            switch (dequeCommand){
+                case 1:
+                    if (!deque.isFull()) {
+                        deque.insertLeft(getInt());
+                    } else {
+                        System.out.println("Deque is full!");
+                    }
+                    break;
+                case 2:
+                    if (!deque.isEmpty()) {
+                        System.out.println(deque.removeLeft());
+                    } else {
+                        System.out.println("Deque is empty!");
+                    }
+                    break;
+                case 3:
+                    if (!deque.isFull()) {
+                        deque.insertRight(getInt());
+                    } else {
+                        System.out.println("Deque is full!");
+                    }
+                    break;
+                case 4:
+                    if (!deque.isEmpty()) {
+                        System.out.println(deque.removeRight());
+                    } else {
+                        System.out.println("Deque is empty!");
+                    }
+                    break;
+                case 5:
+                    System.exit(0);
+            }
+            System.out.println(deque.toString());
+        }
+    }
+    /**
+     * Метод заполняет очередь и забирает из нее значения при помощи консоли
+     */
+    static  void  runQueue(){
+        Queue queue = new Queue(10);
+        while (true){
+            int queueCommand = getCommand("1-Insert Element, 2-Remove element, 3 - Exit", 3);
+            switch (queueCommand){
+                case 1:
+                    if (!queue.isFull()){
+                        queue.insert(getInt());
+                    } else {
+                        System.out.println("Queue is full!");
+                    }
+                    break;
+                case 2:
+                    if (!queue.isEmpty()) {
+                        System.out.println(queue.remove());
+                    } else {
+                        System.out.println("Queue is empty!");
+                    }
+                    break;
+                case 3:
+                    System.exit(0);
+            }
+            System.out.println(queue.toString());
+        }
+    }
 
+    /**
+     * Метод заполняет стек и забирает из него значения при помощи консоли
+     */
+    static void runStack(){
+        Stack stack = new Stack(10);
+        while (true){
+            int stackCommand = getCommand("1-Push Element, 2-Pop element, 3 - Exit", 3);
+            switch (stackCommand){
+                case 1:
+                    if (!stack.isFull()) {
+                        stack.push(getInt());
+                    } else {
+                        System.out.println("Stack is full!");
+                    }
+                    break;
+                case 2:
+                    if (!stack.isEmpty()){
+                        System.out.println(stack.pop());
+                    }
+                    else {
+                        System.out.println("Stack is empty!");
+                    }
+                    break;
+                case 3:
+                    System.exit(0);
+            }
+            System.out.println(stack.toString());
+        }
+    }
+
+    /**
+     * Метод переворасивает строку при помощи класса CharStack. Можно было все сделать при помощи StringBuilder,
+     * но ,если я правильно понял, смысл урока был именно в использовании стека.
+     */
+    static void runStringReverse(){
+        while (true) {
+            int command = getCommand("1-Reverse string, 2 - Exit", 2);
+            switch (command) {
+                case 1:
+                    Scanner sc = new Scanner(System.in);
+                    System.out.println("Enter the string");
+                    String string = new String(sc.nextLine());
+                    CharStack charStack = new CharStack(string.length());
+                    for (int i = 0; i < string.length(); i++) {
+                        charStack.push(string.charAt(i));
+                    }
+                    for (int i = 0; i < string.length(); i++) {
+                        System.out.print(charStack.pop());
+                    }
+                    System.out.println();
+                    break;
+
+                case 2:
+                    System.exit(0);
+            }
+        }
+    }
 }
