a3c19df18f7c47a64312a0ee38151fb9b35868f6
eapat <eapat@mail.ru>
2018-10-12 17:05:10 +0500

    ДЗ4

---
+++ src/main/java/Lesson4HW/LinkIteratorApp.java
@@ -0,0 +1,18 @@
+package Lesson4HW;
+
+public class LinkIteratorApp {
+
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        LinkInterator itr = new LinkInterator(list);
+
+        itr.insertAfter("Artem", 20);
+        itr.insertBefore("Sergey", 10);
+        itr.insertAfter("J", 40);
+
+        list.display();
+    }
+
+}
